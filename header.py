e82b6d9ed2bc98831769dff123918a559ad7ffd3
IvanyshchivAndrii <belka04011993@gmail.com>
2023-09-08 17:41:49 +0300

    inf table little changes

--- header.py
+++ header.py
@@ -4,18 +4,18 @@ from prettytable import PrettyTable
 
 def header_text():
 
-    attention = '*******attention********'.upper()
+    attention = '******* achtung attention увага ********'.upper()
     text_GPA = art.text2art(" GPA", font='lean', chr_ignore=True, space=1)
     text_full = '***** Your Genius Personal Assistant ******'.upper()
 
-    print(f'{attention:^110}\n', text_GPA, f'{text_full:^105}')
+    print(f'{attention:^110}\n', text_GPA, f'{text_full:^105}\n')
 
 
 def inf_table():
     app_table = PrettyTable()
 
-    app_table.field_names = ['№', 'Назва', 'Інформація']
-    app_table.add_row(['1', 'AddressBook', 'Книга контактів для зберігання телефонів, емейлів днів народження та іншої корисної інформації'], divider = True)
+    app_table.field_names = ['№', 'Назва функції', 'Інформація']
+    app_table.add_row(['1', 'AddressBook', 'Книга контактів для зберігання телефонів, емейлів, днів народження та іншої корисної інформації'], divider = True)
     app_table.add_row(['2', 'NoteBook',
                        'Ззаписник в який можна додавати необхідні повсякденні записи'], divider=True)
     app_table.add_row(['3', 'FileSorter',
@@ -23,6 +23,9 @@ def inf_table():
                       divider=True)
     app_table.add_row(['4', 'GPT навігатор',
                        'Допомагає витягти будь яку інформацію збережену в AddressBook чи NoteBook'])
+
+    app_table.max_width['Інформація'] = 60
+
     print(app_table)
 
 
